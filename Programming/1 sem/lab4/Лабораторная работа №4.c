b706f8fc8cbb89e71808ee6744c3680e346d21b0
Asanasm <91276049+AnastasiyaSinuta@users.noreply.github.com>
2022-06-29 16:50:19 +0300

    Add files via upload

---
+++ Programming/1 sem/lab4/Лабораторная работа №4.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+int main()
+{
+    char target[9] = "ITMO";
+    char source[] = "FBIT";
+    char* tptr = target;
+    char* sptr = source;
+    while (*tptr != '\0')   tptr++;
+    while (sptr < (source + sizeof(source)))    *tptr++ = *sptr++;
+    printf("%s\n", target);
+    printf("%d", *(tptr - 2));
+}
