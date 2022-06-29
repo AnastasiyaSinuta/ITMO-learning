dbc01ea77a7eba7dd810bb2af8e3496a627d2fe0
Asanasm <91276049+AnastasiyaSinuta@users.noreply.github.com>
2022-06-29 17:08:47 +0300

    Add files via upload

---
+++ Programming/2 sem/test1/Рубежное тестирование 1.cpp
@@ -0,0 +1,13 @@
+﻿DecStr::DecStr(char ch) : Stroka(ch)
+{
+	if (!(pCh[0] >= '0' && pCh[0] <= '9'))
+	{
+		cout << "Bad symbol!, pCh[0] = " << pCh[0] << endl;
+		if (pCh) delete[] pCh;
+		len = 0;
+		pCh = new char[len + 1];
+		pCh[0] = '\0';
+		return;
+	}
+	cout << "DecStr::DecStr(char ch) : Stroka(ch)" << endl;
+}
