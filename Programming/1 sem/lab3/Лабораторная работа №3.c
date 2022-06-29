86b711db2ae14c81b5cd27ff65144d5c363ef846
Asanasm <91276049+AnastasiyaSinuta@users.noreply.github.com>
2022-06-29 16:49:01 +0300

    Add files via upload

---
+++ Programming/1 sem/lab3/Лабораторная работа №3.c
@@ -0,0 +1,50 @@
+﻿/*Лабораторная работа №3*/
+#include <stdio.h>
+int main()
+{	
+	int A[3][3] = { {96, 72, 38}, {11, 29, 25}, {47, 71, 34} };
+	int sum = 0;
+	for (int i = 0; i < 3; i++, sum = 0)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%d ", A[i][j]);
+			sum += A[i][j];
+		}
+		printf("%s %d \n", "	sum = ", sum);
+	}
+	printf("\n");
+
+	for (int i = 1; i < 3; i++)
+	{
+		for (int m = 0; m < 3 - i; m++)
+		{
+			int sumj = 0;
+			for (int j = 0; j < 3; j++) sumj += A[m][j];
+			int sumk = 0;
+			for (int k = 0; k < 3; k++) sumk += A[m + 1][k];
+			if (sumj > sumk)
+			{
+				int tmp[3] = { 0, 0, 0 };
+				for (int n = 0; n < 3; n++)
+				{
+					tmp[n] = A[m][n];
+					A[m][n] = A[m + 1][n];
+					A[m + 1][n] = tmp[n];
+				}
+			}
+		}
+	}
+
+	sum = 0;
+	for (int i = 0; i < 3; i++, sum = 0)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			printf("%d ", A[i][j]);
+			sum += A[i][j];
+		}
+		printf("%s %d \n", "	sum = ", sum);
+	}
+	return 0;
+}
